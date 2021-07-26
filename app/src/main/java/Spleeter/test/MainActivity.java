f63b433146b303503577e8965a80641bbca45041
oluwabajio <ismailosunlana@yahoo.com>
2021-07-26 23:35:03 +0100

    Playing noise

--- app/src/main/java/Spleeter/test/MainActivity.java
+++ app/src/main/java/Spleeter/test/MainActivity.java
@@ -185,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        byte[] sampleFloatArray = Arrays.copyOfRange(byteArray, 0, 320000);
+        byte[] sampleFloatArray = Arrays.copyOfRange(byteArray, 0, 640000);
 
 
         float[] fArr = byteToFloat(byteArray);
@@ -198,25 +198,25 @@ public class MainActivity extends AppCompatActivity {
         DataType imageDataType  = tflite.getInputTensor(0).dataType();
         Log.e(TAG, "mp3ToFloat: Float Array Size is "+fArr.length );
 
-        tflite.resizeInput(0, new int[]{160000, 2});
+        tflite.resizeInput(0, new int[]{320000, 2});
 
 
 
-        float floats[] = new float[320000];
-        for (int i = 0; i < sampleFloatArray.length-40; i+=2) {// loop thrugh the 128 float arrays, each float array is of size 129
+        float floats[] = new float[640000];
+        for (int i = 0; i < sampleFloatArray.length-4; i+=2) {// loop thrugh the 128 float arrays, each float array is of size 129
             float a0 = sampleFloatArray[i];
             float a1 = sampleFloatArray[i+1];
 //            Log.e(TAG, "mp3ToFloat: float[0] = "+ floats[0] );
 //            Log.e(TAG, "mp3ToFloat: float[1] = "+ floats[1] );
             floats[i/2] = a0;
-            floats[160000+(i/2)] = a1;
+            floats[320000+(i/2)] = a1;
 
         }
 
-        ByteBuffer byteBuffer = ByteBuffer.allocate(160000 *2* 4);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(320000 *2* 4);
 
         TensorBuffer tensorBuffer = TensorBuffer.createDynamic(imageDataType);
-        tensorBuffer.loadArray(floats, new int[]{160000, 2});
+        tensorBuffer.loadArray(floats, new int[]{320000, 2});
         ByteBuffer inByteBuffer = tensorBuffer.getBuffer();
         byteBuffer.put(inByteBuffer);
         byteBuffer.rewind();
@@ -224,8 +224,8 @@ public class MainActivity extends AppCompatActivity {
         Object[] input = new Object[1];
         input[0] = byteBuffer;
 
-        ByteBuffer instrumentalBuffer = ByteBuffer.allocateDirect(1280000);
-        ByteBuffer vocalBuffer = ByteBuffer.allocateDirect(1280000);
+        ByteBuffer instrumentalBuffer = ByteBuffer.allocateDirect(2560000);
+        ByteBuffer vocalBuffer = ByteBuffer.allocateDirect(2560000);
 
         Map outputs = new TreeMap<>();
         outputs.put(0, instrumentalBuffer);
